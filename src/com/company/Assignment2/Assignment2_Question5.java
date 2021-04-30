4ee6ca4fcb946365f8089a9f8130305810d15c68
PratikGholap <pratikgholap@NetworkKiller>
2021-04-29 23:13:11 -0400

    Assignment 2 5th question solved

--- src/com/company/Assignment2/Assignment2_Question5.java
+++ src/com/company/Assignment2/Assignment2_Question5.java
@@ -1,4 +1,21 @@
 package com.company.Assignment2;
 
+import java.util.Scanner;
+
 public class Assignment2_Question5 {
+    public static void main(String[] args) {
+        int num;
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        int index = Integer.toString(num).length();
+        int result = 0, total_count = index;
+        while(num != 0){
+            int rem = num % 10 ;
+            result += (int)Math.pow(10,total_count-rem)*index;
+            index--;
+            num /= 10;
+        }
+        System.out.println("Reverse No.: "+result);
+
+    }
 }
